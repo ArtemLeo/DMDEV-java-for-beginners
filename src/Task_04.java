2e3d3af4c12601c0ec06257a4789a5a4aa56e8e9
Artem Andriushchenko <link.by.artem@gmail.com>
2023-07-06 07:57:04 +0300

    add task 4

--- src/Task_04.java
+++ src/Task_04.java
@@ -11,12 +11,22 @@ import java.util.Scanner;
 public class Task_04 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int year = scanner.nextInt();
+        isLeapYearVOID(scanner.nextInt());
+        System.out.println("Високосный - " + isLeapYearRETURN(scanner.nextInt()));
 
+
+    }
+
+    public static void isLeapYearVOID(int year) {
         if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
-            System.out.println(year + " високосный");
+            System.out.println(year + " - високосный");
         } else {
-            System.out.println(year + " не високосный");
+            System.out.println(year + " - не високосный");
         }
     }
+
+    public static boolean isLeapYearRETURN(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+    }
 }
+
