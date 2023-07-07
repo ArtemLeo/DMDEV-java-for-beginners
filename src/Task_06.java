17a27c345445171ebeadea4ad957f6baa23ab815
Artem Andriushchenko <link.by.artem@gmail.com>
2023-07-07 20:25:56 +0300

    add task 7

--- src/Task_06.java
+++ src/Task_06.java
@@ -7,6 +7,7 @@ public class Task_06 {
     public static void main(String[] args) {
         int value = 3;
         System.out.println("Cube is: " + cube(value));
+
     }
 
     public static int cube(int value) {
---
+++ src/Task_07.java
@@ -0,0 +1,31 @@
+
+/*
+ * Написать программу, вычисляющую факториал введённого целого числа;
+ */
+
+public class Task_07 {
+    public static void main(String[] args) {
+        int number = 5;
+        System.out.println("Factorial of number " + number + " = " + factorial(number));
+
+        System.out.println("Factorial of number " + number + " = " + factorialWhile(number));
+    }
+
+    public static int factorial(int value) {
+        int result = 1;
+        for (int i = 1; i <= value; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    public static int factorialWhile(int value) {
+        int result = 1;
+        int currentValue = 1;
+        while (currentValue <= value) {
+            result *= currentValue;
+            currentValue++;
+        }
+        return result;
+    }
+}
