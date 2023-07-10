eeb7d3121bcd3a5c0b03e7c3fdcbafd2884792b2
Artem Andriushchenko <link.by.artem@gmail.com>
2023-07-10 16:12:38 +0300

    add task 16

---
+++ src/Task_16.java
@@ -0,0 +1,34 @@
+
+/*
+ * Написать 2-ва метода, которые определяют индекс минимального и максимального элемента в одномерном массиве.
+ */
+
+public class Task_16 {
+    public static void main(String[] args) {
+        int[] array = {3, 76, 36, 0, 76, -5, 98, 3, -7, 54, 17, -8, 35};
+        System.out.println("Index of MaxValue in array = " + valueMax(array));
+        System.out.println("Index of MinValue in array = " + valueMin(array));
+        System.out.println("Max value: " + array[6]);
+        System.out.println("Min value: " + array[11]);
+    }
+
+    private static int valueMin(int[] array) {
+        int minIndex = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[minIndex] > array[i]) {
+                minIndex = i;
+            }
+        }
+        return minIndex;
+    }
+
+    private static int valueMax(int[] array) {
+        int maxIndex = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[maxIndex] < array[i]) {
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
+    }
+}
